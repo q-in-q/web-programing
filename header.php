3ddef89379a74d53a2378a165b1410dfef2a783d
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-06-21 23:02:05 +0700

    Update header.php

--- header.php
+++ header.php
@@ -57,7 +57,7 @@
                                 </div>
                                 
                             </li>
-                            <li class="nav-link"><a href="our-team.php">Our Team</a></li>
+                            <li class="nav-item"><a class="nav-link"  href="our-team.php">Our Team</a></li>
                         </ul>
                         
                     </div>
