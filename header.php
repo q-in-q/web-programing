465dd751a31e05a97c4f51ce0067451ab4466e89
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-06-21 22:52:37 +0700

    Update header.php

--- header.php
+++ header.php
@@ -47,7 +47,7 @@
                         </button>
                     
                         <div class="collapse navbar-collapse">
-                        <ul class="navbar-nav mr-auto navigasi">  
+                        <ul class="navbar-nav mr-auto navigasi justify-content-end">  
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="index.php">
                                     Home
@@ -58,10 +58,7 @@
                             </li>
                             <li class="nav-link"><a href="our-team.php">Our Team</a></li>
                         </ul>
-                        <form class="form-inline my-2 my-lg-0">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                        </form>
+                        
                     </div>
                 </nav>
             </div>
