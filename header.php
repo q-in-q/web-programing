fece8a4fc309dafab04eb0ff76e0987aa6100ae2
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-06-21 22:47:06 +0700

    Update header.php

--- header.php
+++ header.php
@@ -49,7 +49,7 @@
                         <div class="collapse navbar-collapse">
                         <ul class="navbar-nav mr-auto navigasi">  
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <a class="nav-link dropdown-toggle" href="index.php">
                                     Home
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
