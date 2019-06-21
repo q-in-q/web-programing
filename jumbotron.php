5bada421ee80f078449c150c478a67501a468ca1
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-06-22 02:22:03 +0700

    Update jumbotron.php

--- jumbotron.php
+++ jumbotron.php
@@ -1,7 +1,9 @@
-<div class="jumbotron jumbotron-child jumbotron-fluid">
-  <div class="container text-center">
-    <h1>We are not Perfect Without Team</h1>
-    <h4>keluarga, sahabat, dan orang tersayang</h4>
-    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+<div class="jumbotron jumbotron-fluid">
+  <div class="jumbotron-child-2 ">
+      <div class="container text-center">
+            <h1>Sambut Kemenangan #Bersam4th ITConsultant</h1>
+            <h4>nikmati fi</h4>
+            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+      </div>
   </div>
 </div>
