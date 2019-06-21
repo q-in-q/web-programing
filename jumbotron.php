d1bac8f8b3c70833b094f35a22446b0f20f0c114
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-06-22 02:07:45 +0700

    Update jumbotron.php

--- jumbotron.php
+++ jumbotron.php
@@ -1,6 +1,7 @@
-<div class="jumbotron jumbotron-fluid">
-  <div class="container ">
-    <h1>Fluid jumbotron</h1>
+<div class="jumbotron jumbotron-child jumbotron-fluid">
+  <div class="container text-center">
+    <h1>We are not Perfect Without Team</h1>
+    <h4>keluarga, sahabat, dan orang tersayang</h4>
     <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
   </div>
 </div>
