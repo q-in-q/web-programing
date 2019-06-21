bde8bc8eee58127ab0ca66719b1c574fe8c5cc8f
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-06-21 22:21:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,46 +12,46 @@
             </div>
         </section>
         <section class="grid-icon">
-	    <div class="container">
-	        <div class="section-title">
-                <h2>The Company. Our Values. Our Beliefs.</h2>
-	        </div>
-	        <div class="row fitur-detail section-box">
-              	<div class="col-md-4 ">
-               		<div class="row section-box-1">
-               			<div class="col-md-4 section-box-icon">
-			               		<i class="fas fa-search fa-3x circle-icon"></i>
-		               	</div>
-	               		<div class="col-md-8 section-box-content">
-	                   		<h3>SPECIFIC</h3>
-	                   		<p>Menerangkan dengan mudah dan cepat.</p>
-	                   	</div>
-	                </div>
+            <div class="container">
+                <div class="section-title">
+                    <h2>The Company. Our Values. Our Beliefs.</h2>
                 </div>
-                <div class="col-md-4">
-	               	<div class="row section-box-1">
-                		<div class="col-md-4 section-box-icon">
-			               		<i class="far fa-file-alt fa-3x circle-icon"></i>
-		               	</div>
-	               		<div class="col-md-8 section-box-content">
-	                   		<h3>MEASURABLE</h3>
-	                       		<p>Memberikan mamfaat yang nyata dan terukur.</p>
-	                    	</div>
-	                    </div>
+                <div class="row fitur-detail section-box">
+                    <div class="col-md-4 ">
+                        <div class="row section-box-1">
+                            <div class="col-md-4 section-box-icon">
+                                <i class="fas fa-search fa-3x circle-icon"></i>
+                            </div>
+                            <div class="col-md-8 section-box-content">
+                                <h3>SPECIFIC</h3>
+                                <p>Menerangkan dengan mudah dan cepat.</p>
+                            </div>
+                        </div>
                     </div>
                     <div class="col-md-4">
-	                	<div class="row section-box-1">
-                			<div class="col-md-4 section-box-icon">
-			                		<i class="fas fa-bullseye fa-3x circle-icon"></i>
-		                	</div>
-	                		<div class="col-md-8 section-box-content">
-	                    		<h3>ACURATE</h3>
-	                    		<p>Benar dalam semua rincian melalui metode yang transparan.</p>
-	                    	</div>
-	                    </div>
+                        <div class="row section-box-1">
+                            <div class="col-md-4 section-box-icon">
+                                    <i class="far fa-file-alt fa-3x circle-icon"></i>
+                            </div>
+                            <div class="col-md-8 section-box-content">
+                                <h3>MEASURABLE</h3>
+                                    <p>Memberikan mamfaat yang nyata dan terukur.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="row section-box-1">
+                                <div class="col-md-4 section-box-icon">
+                                        <i class="fas fa-bullseye fa-3x circle-icon"></i>
+                                </div>
+                                <div class="col-md-8 section-box-content">
+                                    <h3>ACURATE</h3>
+                                    <p>Benar dalam semua rincian melalui metode yang transparan.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-	            </div>
-            </div>
+                </div>
         </section>
         <!-- SELAYANG PANDANG -->
         <section class="selayang-pandang">
