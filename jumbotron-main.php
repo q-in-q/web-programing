8cf7c0e0046c7d681dd4cbffde3edc9b132f6d69
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-06-22 02:22:07 +0700

    Create jumbotron-main.php

---
+++ jumbotron-main.php
@@ -0,0 +1,9 @@
+<div class="jumbotron jumbotron-fluid">
+  <div class="jumbotron-child-1 ">
+      <div class="container text-center">
+            <h1>We are not Perfect Without Team</h1>
+            <h4>keluarga, sahabat, dan orang tersayang</h4>
+            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+      </div>
+  </div>
+</div>
