4548d49f232b674c3418c65584cc1ae059204fdd
Alvin Adwitya <alvinadwitya.id@gmail.com>
2019-06-22 23:19:57 +0700

    update js

--- assets/js/makeJavascript.js
+++ assets/js/makeJavascript.js
@@ -38,3 +38,17 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 50) {
+            $("#nav").css("background-color" , "#06061fa1");
+            $("#nav").css("transition" , ".3s");
+        }
+
+        else{
+            $("#nav").css("background" , "transparent");  	
+        }
+    });
+    });
+
--- header.php
+++ header.php
@@ -14,7 +14,6 @@
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/popper.min.js"></script>
-    <script src="assets/js/js.js"></script>
 
     <title>HOME | My Consultant</title>
     
