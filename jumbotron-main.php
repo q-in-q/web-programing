850b790b50bc738b69568d01ea017ab022e1c778
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-06-22 23:28:43 +0700

    Update jumbotron-main.php

--- jumbotron-main.php
+++ jumbotron-main.php
@@ -3,7 +3,7 @@
       <div class="container jumbotron-content-main">
             <h1>Sambut Kemenangan #Bersam4th ITConsultant.com</h1>
             <h4>Nikmati Pelayanan Terbaik, Ciptakan Suksesmu disini!</h4>
-            <button class="btn btn-primary"><a href="#">PROMO DISKON 50%</a></button>
+            <button class="btn btn-primary"><a href="promo.php">PROMO DISKON 50%</a></button>
       </div>
   </div>
 </div>
