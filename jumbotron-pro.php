b472be7ae0362ece41d0f9c2b8bb0d870223fa16
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-06-22 23:28:40 +0700

    Delete jumbotron-pro.php

--- jumbotron-pro.php
+++
@@ -1,7 +0,0 @@
-<div class="jumbotron jumbotron-fluid">
-  <div class="jumbotron-child-pro">
-      <div class="container jumbotron-content-main text-center">
-            <h1>Nimkati Pelayanan VIP dari Kami</h1>
-      </div>
-  </div>
-</div>
