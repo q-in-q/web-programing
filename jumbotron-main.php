9cdcd3ae2c7e35364b91aad7c7218248689b9a7d
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-06-22 09:50:20 +0700

    Update jumbotron-main.php

--- jumbotron-main.php
+++ jumbotron-main.php
@@ -1,8 +1,8 @@
 <div class="jumbotron jumbotron-fluid">
-  <div class="jumbotron-child-1 ">
-      <div class="container text-center">
-            <h1>We are not Perfect Without Team</h1>
-            <h4>keluarga, sahabat, dan orang tersayang</h4>
+  <div class="jumbotron-child-1">
+      <div class="container jumbotron-content-main">
+            <h1>Sambut Kemenangan #Bersam4th IDCloudHost</h1>
+            <h4>Nikmati Pelayanan Terbaik, Ciptakan Suksesmu disini!</h4>
             <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
       </div>
   </div>
