61fbeec0874b747850bb10dc02cdfaf89d0a2f5a
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-06-22 22:22:42 +0700

    Update jumbotron-main.php

--- jumbotron-main.php
+++ jumbotron-main.php
@@ -1,7 +1,7 @@
 <div class="jumbotron jumbotron-fluid">
   <div class="jumbotron-child-1">
       <div class="container jumbotron-content-main">
-            <h1>Sambut Kemenangan #Bersam4th IDCloudHost</h1>
+            <h1>Sambut Kemenangan #Bersam4th ITConsultant.com</h1>
             <h4>Nikmati Pelayanan Terbaik, Ciptakan Suksesmu disini!</h4>
             <button class="btn btn-primary"><a href="#">PROMO DISKON 50%</a></button>
       </div>
