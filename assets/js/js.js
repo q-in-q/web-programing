2759bb9926c05897a5fcd4859bb5987f0573b2ba
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-06-22 23:01:56 +0700

    Update js.js

--- assets/js/js.js
+++ assets/js/js.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
         if (scroll > 50) {
-            $("#nav").css("background-color" , "#555");
+            $("#nav").css("background-color" , "#06061fa1");
+            $("#nav").css("transition" , ".3s");
         }
 
         else{
