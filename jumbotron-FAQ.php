89bc4f398aaa2b35ae398840c93169c98d1bef4f
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-06-27 10:39:41 +0700

    Create jumbotron-FAQ.php

---
+++ jumbotron-FAQ.php
@@ -0,0 +1,7 @@
+<div class="jumbotron jumbotron-fluid">
+  <div class="jumbotron-child-pro">
+      <div class="container jumbotron-content-main text-center">
+            <h1>Frequently Asked Question</h1>
+      </div>
+  </div>
+</div>
