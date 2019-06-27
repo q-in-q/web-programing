8b2f288fa523a3585be8a49d928a1f7dad0b619d
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-06-27 13:16:42 +0700

    Create artikel-ketentuan-diskon.php

---
+++ artikel-ketentuan-diskon.php
@@ -0,0 +1,65 @@
+<?php
+include("header.php");
+include("jumbotron-promo.php");
+?>
+<div class="container">
+    <div>
+        <div class="row">
+            <div class="col">
+                <h1>KETENTUAN PROMO</h1>
+                <table>
+                    <tr>
+                        <td>No</td>
+                        <td>Ketentuan</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Promo hanya berlaku untuk WNI</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Hanya berlaku untuk member</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Harus menunjukan kartu identitas</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>Pembayaran menggunakan kartu kredit akan mendapat cashback sebesar 10%</td>
+                    </tr>
+                    <tr>
+                        <td>5</td> 
+                        <td>Promo berlaku mulai tanggal 22 juni-22 juli 2019</td>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td>Promo berlaku untuk semua cabang kami di wilayah Jogja</td>
+                    </tr>
+                    <tr>
+                        <td>7</td>
+                        <td>Promo hanya berlaku untuk barang yang bertanda</td>
+                    </tr>
+                    <tr>
+                        <td>8</td>
+                        <td>Barang yang sudah di bayar tidak bisa di kembalikan dengan alasan apapun</td>
+                    </tr>
+                    <tr>
+                        <td>9</td>
+                        <td>Tidak ada batas minimum pembelian</td>
+                    </tr>
+                    
+                </table>
+            </div>
+            <div class="col">
+                <img class="img-promo" src="assets/img/PROMO.png" alt="">
+            </div>
+        </div>
+        
+    </div>
+</div>
+
+
+<?php
+include("footer.php");
+?>
