76cf1a4ec9ad08661fe81db92b44e28c24194451
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-06-27 13:16:24 +0700

    Create faq.php

---
+++ faq.php
@@ -0,0 +1,50 @@
+<?php
+include("header.php");
+include("jumbotron-FAQ.php");
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>FAQ</h1>
+            <table>
+                <tr>
+                    <td>No</td>
+                    <td>Pertanyaan</td>
+                    <td>Jawaban</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Bagaimana cara saya untuk mengirimkan lowongan pekerjaan?</td>
+                    <td>Pertama , Anda akan perlu untuk mendaftar sebagai Employer. Karena kami saat ini sedang dalam proses untuk meningkatkan produk dan layanan kami , Konsultan Bisnis kami akan membantu Anda dalam posting peluang karir di website kami. </td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Apa misi perusahaan anda?</td>
+                    <td>Misi kami sebagai perusahaan internasional yang mampu memberikan solusi terdepan dalam dunia sipil konstruksi, geoteknik dan lingkungan.</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Apakah ide bisnis perusahaan ini?</td>
+                    <td>Kami menawarkan berbagai macam rancangan yang dirancang dengan baik, produk perabotan rumah fungsional dengan harga yang sangat rendah sehingga sebanyak mungkin orang akan mampu membelinya.</td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>Dapatkah saya mendistribusikan produk perusaaan ini?</td>
+                    <td> Agar konsep dapat bekerja, dibutuhkan untuk memiliki semua aspek di satu tempat. Oleh karena itu, produk kami hanya dijual di toko-toko yang telah bekerja sama dengan kami.</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+        <div class="col">
+            <img class="img-promo" src="./assets/img/consultant.jpg" alt="">
+        </div>
+    </div>
+</div>
+<?php
+include("footer.php");
+?>
