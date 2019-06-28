7ce71e2bea388e5111c86ae615813837c81bd036
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-06-28 11:01:11 +0700

    Update success.php

--- success.php
+++ success.php
@@ -6,6 +6,19 @@ if($_SESSION['first_name']=='')
 }
 ?>
  
-Selamat Datang Users <?php echo $_SESSION['first_name']?>
- <br>
-<a href="logout.php">Logout</a>
+<html>
+    <head>
+        <style type="text/css">
+            h1, a {
+                text-align: center;
+                margin: 10px 0;
+            }
+        </style>
+    </head>
+    <body>
+    <h1>Selamat Datang Users <b><?php echo $_SESSION['first_name']?></b> </h1>
+    <br>
+    <a href="logout.php">Logout</a>
+
+    </body>
+</html>
