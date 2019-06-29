6fa97a0dac163df4d4639f2a55412c75926a28e4
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-06-29 20:11:46 +0700

    Update

--- promo-detail.php
+++ promo-detail.php
@@ -2,6 +2,14 @@
 include("header.php");
 include("jumbotron-promo-detail.php");
 ?>
+<div class="container">
+    <div class="main-promo-detail text-center">
+        <img src="./assets/img/Promo-Domain-ID-Juni-2019-IDCloudHost.jpg" alt="">
+        <p>IDcloudhost merupakan salah satu REGISTRAR Resmi dari PANDI untuk Domain Indonesia. Oleh karena itu kami senantiasa akan terus memberikan kepercayaan kepada Anda untuk memberikan layanan Domain Indonesia dengan harga yang terjangkau dan Proses yang sangat mudah untuk bisa mengamankan Brand Domain Indonesia Anda. Yuk Cinta domain Indonesia mulai saat ini, Kita memberikan penawaran Promo Domain ID Super murah Se-Indonesia Beli dan Transfer dengan Harga Rp 107.000 untuk setahun!.</p>
+
+        <p>Lihat syarat dan ketentuan Promo <span> <a href="syarat-ketentuan-diskon.php">di sini</a> </span> </p>
+    </div>
+</div>
 <?php 
 include("footer.php");
 ?>
