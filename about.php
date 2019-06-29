ed1a677f8b0bf3bcbbfe06f60d1c140ffe9155e3
Alvin Adwitya <alvinadwitya.id@gmail.com>
2019-06-29 22:55:07 +0700

    Update about.php

--- about.php
+++ about.php
@@ -1,44 +1,35 @@
 <?php 
-    include("header.php");
-    include("jumbotron.php");
+include("header.php");
+include("jumbotron.php");
 ?>
 
-<html>
-    <head>
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=PT+Sans">
-        <link rel="stylesheet" href="assets/css/style.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    </head>
-    <body>
-        <div class="navbar-container">
-                <nav>
-                    <ul>
-                        <a href="company.php"><li class="tab-click">Company</li></a>
-                        <a href="our-team.php"><li>Our Team</li></a>
-                        <a href="#"><li>Download</li></a>
-                    </ul>
-                </nav>
-            </div>
-        <section class="about">
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <div class="judul">
-                            <h3>PT MITRA TECHNOLOGY</h3>
-                            <p>PT Cloud Hosting Indonesia (IDCloudHost) is among Indonesian Web Hosting Provider that offers services such as Domain Registration, Cloud Hosting, Server (VPS & Dedicated Server), Reseller Domain & Hosting, and others.</p>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="judul">
-                            <h3>PT MITRA TECHNOLOGY</h3>
-                        </div>
-                    </div>
+<div class="navbar-container">
+        <nav>
+            <ul>
+                <a href="company.php"><li class="tab-click">Company</li></a>
+                <a href="our-team.php"><li>Our Team</li></a>
+                <a href="#"><li>Download</li></a>
+            </ul>
+        </nav>
+    </div>
+<section class="about">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="judul">
+                    <h3>PT MITRA TECHNOLOGY</h3>
+                    <p>PT Mitra Technology Merupakan Sebuah Perusahan Software House yang Bergerak di Bidang Technology Kami Menawarkan Layanan Software Development, Web Development, IT Consultant, SEO Analis, Sosial Media Marketing, Penetration Testing</p>
                 </div>
-                
             </div>
-        </section>
-    </body>
-</html>
+            <div class="col">
+                <div class="judul">
+                    <h3>PT MITRA TECHNOLOGY</h3>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</section>
 
 <?php
     include("footer.php");
