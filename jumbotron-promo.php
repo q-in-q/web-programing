aa334b9d5711276213aad931950c8b0b48042f81
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-06-30 23:34:54 +0700

    update

--- jumbotron-promo.php
+++ jumbotron-promo.php
@@ -2,6 +2,7 @@
   <div class="jumbotron-child-pro">
       <div class="container jumbotron-content-main text-center">
             <h1>PROMO</h1>
+            <p>Dapatkan Penawaran Promo Menarik, Jangan sampai kelewatan!</p>
       </div>
   </div>
 </div>
