2b258a58268e9844802e707477821dfd32b74445
Anang Wahyu P <anangwahyupradanaofficial@gmail.com>
2019-07-02 23:41:48 +0700

    Download

--- download.php
+++ download.php
@@ -17,19 +17,30 @@ include("jumbotron.php");
         <section class="team">
             <div class="container">
                 <div class="row download">
-                    <div class="col">
+                    <div class="col download-judul">
                         <h1>Dapatkan Buku Ekslusuf Kami Disini</h1>
-                        <h3>Ebook: Client Getting Strategies</h3>
+                        <h3>Ebook : Client Getting Strategies</h3>
                         <div class="price">
-                            <p>free</p>
+                            <p>* Free</p>
                         </div>
                         <div class="download-deskripsi">
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt nostrum, explicabo magnam repellendus harum iure eveniet nemo repellat aut hic, officia quia debitis eligendi? Atque dignissimos totam unde sunt error?</p>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                            <button class="btn-grad btn-grad-2">DOWNLOAD <i class="fas fa-cart-arrow-down"></i></button>
+                            </div>
+                            <div class="col">
+                            <button class="btn-grad btn-grad-3"><i class="fas fa-hand-holding-usd"></i> DONATE</button>
+                            </div>
                         </div>
                     </div>
                     <div class="col thumbnail">
                         <img src="./assets/img/Client-Getting-Strategies-Ebook-Thumb.png">
                     </div>
+                </div>
+                <div class="text-center download-more">
+                    <p><a href="#">Selengkapnya</a></p>
                 </div>
             </div>
         </section>
